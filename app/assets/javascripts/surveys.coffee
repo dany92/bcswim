4a18a0bfbf7dfea9a3f1ce2deb55933b6d1fcde6
Iris <dany@bc.edu>
2015-08-14 23:43:39 -0500

    Survey Deletion works

--- app/assets/javascripts/surveys.coffee
+++ app/assets/javascripts/surveys.coffee
@@ -1,2 +1,2 @@
 jQuery ->
-$('#survey_date').datepicker({dateFormat:'yy-mm-dd’})
+$('#survey_date').datepicker()
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,11 @@
 class Question < ActiveRecord::Base
+	has_and_belongs_to_many :surveys
 	before_create :generate_token
  	protected
 =begin
  	Use hashing instead later.
  	This does not generate a unique value
 =end
-	has_and_belongs_to_many :surveys
  	def generate_token
 		self.num=rand(999999)
     end
